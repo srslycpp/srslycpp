1af20d87c5e7506b7b2b44e201c34a6e0237eed3
srslycpp <mvcc@op.pl>
2018-09-01 17:37:01 +0200

    adding image

---
+++ myWeb/src/main/java/pl/srslycpp/myWeb/Service/ImageService.java
@@ -0,0 +1,11 @@
+package pl.srslycpp.myWeb.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+
+}
---
+++ myWeb/src/main/java/pl/srslycpp/myWeb/Service/ImageServiceImpl.java
@@ -0,0 +1,17 @@
+package pl.srslycpp.myWeb.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+
+        @Override
+        public void saveImageFile(Long recipeId, MultipartFile file) {
+
+            log.debug("Received a file");
+
+        }
+}
