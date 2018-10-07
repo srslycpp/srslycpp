ff6d59982df4d2a05e44cc16d7b0ed4832834cef
srslycpp <mvcc@op.pl>
2018-10-07 22:23:29 +0200

    testing

--- myWeb/src/main/java/pl/srslycpp/myWeb/App/ClassTest.java
+++ myWeb/src/main/java/pl/srslycpp/myWeb/App/ClassTest.java
@@ -1,9 +1,40 @@
 package pl.srslycpp.myWeb.App;
 
 public class ClassTest {
-    public static enum EnumTest {
 
-        FALL(1), SUMMER(2), SPRIMG(3), WINTER(4);
+    private int amount;
+    private String currency;
+
+    @Override
+    public String toString() {
+        return "ClassTest{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public ClassTest(String currency, int amount){
+        this.amount=amount;
+        this.currency=currency;
+    }
+
+    public enum EnumTest1{
+        PLN, HRT;
+    }
+
+    public static class AddingAmonut{
+        public static ClassTest add(ClassTest amount1, ClassTest amount2){
+            if(!amount1.currency.equals(amount2.currency)){
+                throw  new RuntimeException("Currency does not match ");
+            }
+            return new ClassTest(amount1.currency, amount1.amount+amount2.amount);
+        }
+    }
+
+
+    public enum EnumTest {
+
+        FALL(1), SUMMER(2), SPRING(3), WINTER(4);
 
         private int code;
 
@@ -24,13 +55,13 @@ public class ClassTest {
                     return 4;
                 case WINTER:
                     return 3;
-                case SPRIMG:
+                case SPRING:
                     return 2;
             }
             return 0;
         }
 
-        public EnumTest valueOf(int in) {
+        public static EnumTest valueOf(int in) {
             for (EnumTest e : EnumTest.values()) {
                 if (e.getCode() == in)
                     return e;
@@ -38,8 +69,11 @@ public class ClassTest {
             throw new RuntimeException(" Ni ma takiego enuma :) ");
         }
 
-        public static void main(String[] args) {
+        public static void main(String[] args)  throws Exception{
 
-        }
+            AddingAmonut add = new AddingAmonut();
+            System.out.println(add.add(new ClassTest("PLN",6),
+                    new ClassTest("PLN",6)).toString());
     }
+}
 }
