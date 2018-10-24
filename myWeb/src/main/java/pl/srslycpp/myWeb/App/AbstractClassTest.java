90edece2178ca73d7a4885e7a1e068b1a7718e9a
srslycpp <mvcc@op.pl>
2018-10-24 22:26:47 +0200

    no change commit

--- myWeb/src/main/java/pl/srslycpp/myWeb/App/AbstractClassTest.java
+++ myWeb/src/main/java/pl/srslycpp/myWeb/App/AbstractClassTest.java
@@ -5,7 +5,6 @@ public abstract class AbstractClassTest {
     public abstract void run();
 
     public void run2(){};
-
     
     AbstractClassTest(){
         System.out.println("abstract Class Test");
