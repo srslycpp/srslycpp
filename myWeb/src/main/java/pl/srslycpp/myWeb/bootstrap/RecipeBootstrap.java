ffb78245ef38c856b718f2f509cce3c5aa194e4e
srslycpp <mvcc@op.pl>
2018-11-20 18:20:06 +0100

    products project

--- myWeb/src/main/java/pl/srslycpp/myWeb/bootstrap/RecipeBootstrap.java
+++ myWeb/src/main/java/pl/srslycpp/myWeb/bootstrap/RecipeBootstrap.java
@@ -22,7 +22,6 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
-
     
     public RecipeBootstrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.categoryRepository = categoryRepository;
