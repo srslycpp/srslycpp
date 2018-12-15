3b46f9980e84b867a516ff4367bde68f668c5f67
srslycpp <mvcc@op.pl>
2018-12-15 17:15:39 +0100

    products project

--- myWeb/src/main/java/pl/srslycpp/myWeb/bootstrap/RecipeBootstrap.java
+++ myWeb/src/main/java/pl/srslycpp/myWeb/bootstrap/RecipeBootstrap.java
@@ -38,6 +38,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         System.out.println("some sout from RecipeBootstrap <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
         recipeRepository.saveAll(getRecipes());
     }
+    
     private List<Recipe> getRecipes() {
 
         List<Recipe> recipes = new ArrayList<>(2);
