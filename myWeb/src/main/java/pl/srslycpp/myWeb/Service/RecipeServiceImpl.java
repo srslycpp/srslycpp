7065545f310ac6558b6ca1eaf16bdc996e019a66
srslycpp <mvcc@op.pl>
2019-03-10 20:42:30 +0100

    products project

--- myWeb/src/main/java/pl/srslycpp/myWeb/Service/RecipeServiceImpl.java
+++ myWeb/src/main/java/pl/srslycpp/myWeb/Service/RecipeServiceImpl.java
@@ -50,6 +50,7 @@ public class RecipeServiceImpl implements RecipeService {
 
         Recipe savedRecipe = recipeRepository.save(detachedRecipe);
         log.debug("Saved RecipeId RecipeCommand saveRecipeCommand: " + savedRecipe.getId());
+        
         return recipeToRecipeCommand.convert(savedRecipe);
     }
     @Override
