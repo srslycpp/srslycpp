24cb4a71c7df38bc83bb7d1748b009bf2830f874
srslycpp <mvcc@op.pl>
2019-04-04 19:33:27 +0200

    products project

--- myWeb/src/main/java/pl/srslycpp/myWeb/RecipeRepository/RecipeRepository.java
+++ myWeb/src/main/java/pl/srslycpp/myWeb/RecipeRepository/RecipeRepository.java
@@ -3,8 +3,6 @@ package pl.srslycpp.myWeb.RecipeRepository;
 import org.springframework.data.repository.CrudRepository;
 import pl.srslycpp.myWeb.RecipeEntity.Recipe;
 
-import java.util.Optional;
-
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-    Optional<Recipe> findByDescription(String description);
+    //Optional<Recipe> findByDescription(String description);
 }
